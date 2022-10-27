d81f6ea750ef25cb5a2e37f8c3e841333473b181
MasterTeam-dev <adjibeadjibe@gmail.com>
2022-10-27 22:56:30 +0200

    kubernetes

---
+++ kubernetes-spring-boot-app/src/main/java/com/isi/kubernetesspringbootapp/KubernetesSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.isi.kubernetesspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesSpringBootAppApplication.class, args);
+	}
+
+}
